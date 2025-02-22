6adb1974bab9c3ff2e98c414b9884fb73ba8cd47
Krish Shah <shahkrish2016@gmail.com>
2025-02-22 17:19:40 -0500

    feat: basic auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,28 @@
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import { ReactNode } from "react";
+
+const AuthProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_CLIENT_ID}>
+      {children}
+    </GoogleOAuthProvider>
+  );
+};
+
 function App() {
-  return <div className="text-xl bg-black text-white">Vite + React</div>;
+  return (
+    <AuthProvider>
+      <GoogleLogin
+        onSuccess={(credentialResponse) => {
+          console.log(credentialResponse);
+        }}
+        onError={() => {
+          console.log("Login Failed");
+        }}
+        useOneTap
+      />
+    </AuthProvider>
+  );
 }
 
 export default App;
